28fbbe9d7c294deb1a6d8c11d78a7eda7a65ce19
abeni-al7 <abenezeralebachew3@gmail.com>
2025-03-31 16:43:24 +0300

    Set app theme

--- app/src/main/java/com/example/a30daysfitness/MainActivity.kt
+++ app/src/main/java/com/example/a30daysfitness/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.a30daysfitness.ui.theme._30DaysFitnessTheme
+import com.example.a30daysfitness.ui.theme.DaysFitnessTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _30DaysFitnessTheme {
+            DaysFitnessTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    _30DaysFitnessTheme {
+    DaysFitnessTheme {
         Greeting("Android")
     }
 }
